2312fa1cd67524e5d62b6b3f461ff1f92bfac7d3
jalkal <jalcalde@gmail.com>
2017-06-18 16:01:19 +0200

    Implement Dummy rest service

--- src/main/java/com/jalkal/TestDockerApplication.java
+++ src/main/java/com/jalkal/TestDockerApplication.java
@@ -1,7 +1,17 @@
 package com.jalkal;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class TestDockerApplication {
@@ -9,4 +19,35 @@ public class TestDockerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestDockerApplication.class, args);
 	}
+}
+
+@RestController
+class ServiceControler{
+
+	@RequestMapping
+	public List<Customer> getAll(){
+
+		return Stream.generate(() -> new Customer(UUID.randomUUID().toString(), getName(), getSurname(), new Random().nextInt(80))).limit(300)
+				.collect(Collectors.toList());
+
+	}
+
+	public String getName(){
+		String[] names = "Pepe,Juan,Antonio,Lucas,Matias,Luis".split(",");
+		return names[new Random().nextInt(names.length)];
+	}
+
+	public String getSurname(){
+		String[] names = "Douglas,Anderson,Smith,Garcia,Lopez,Perez".split(",");
+		return names[new Random().nextInt(names.length)];
+	}
+}
+
+@Data
+@AllArgsConstructor
+class Customer{
+	String id;
+
+	String name, surname;
+	int age;
 }
